09f0fa51284a1d8f051145b70a616b93dce2f8d1
anshu <anshusnp2004@gmail.com>
2026-01-13 00:09:44 +0530

    deploy

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,14 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
-console.log("Cloudinary Config:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET ? "[HIDDEN]" : "NOT SET",
-});
+import dotenv from "dotenv";
+
+dotenv.config();
 
 cloudinary.config({
-  cloud_name:"dxckdnpmi",
-  api_key:"719782311442648",
-  api_secret:"KPEEkfA01ZakBJAZVjJmE7swPrI"
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 export default cloudinary;
